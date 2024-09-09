7f5da947dbbaf4ab7304054cd138bbd2251507a8
qcloud <ubuntu@localhost.localdomain>
2024-09-09 20:55:23 +0800

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,16 @@
-import { getFrameMetadata } from 'frog/next'
-import type { Metadata } from 'next'
-import Image from 'next/image'
+import { getFrameMetadata } from "frog/next";
+import type { Metadata } from "next";
+import Image from "next/image";
 
-import styles from './page.module.css'
+import styles from "./page.module.css";
 
 export async function generateMetadata(): Promise<Metadata> {
   const frameTags = await getFrameMetadata(
-    `${process.env.VERCEL_URL || 'http://localhost:3000'}/api`,
-  )
+    `${process.env.VERCEL_URL || "http://localhost:3000"}/api`
+  );
   return {
     other: frameTags,
-  }
+  };
 }
 
 export default function Home() {
